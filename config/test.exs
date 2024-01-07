ce9352dbd243b66e99b4716ca36e47eeff331800
Raul Pereira <raul.pereira@gmail.com>
2024-01-07 00:11:39 -0300

    add users table, and schema

--- config/test.exs
+++ config/test.exs
@@ -25,3 +25,8 @@ config :logger, level: :warning
 
 # Initialize plugs at runtime for faster test compilation
 config :phoenix, :plug_init_mode, :runtime
+
+# speed up hash in tests
+config :argon2_elixir,
+  t_cost: 1,
+  m_cost: 8
---
+++ lib/banana_bank/users/user.ex
@@ -0,0 +1,34 @@
+defmodule BananaBank.Users.User do
+  use Ecto.Schema
+
+  import Ecto.Changeset
+
+  @fields ~w[name password email cep]a
+  @required_fields @fields
+
+  schema "users" do
+    field :name, :string
+    field :password, :string, redact: true, virtual: true
+    field :password_hash, :string
+    field :email, :string
+    field :cep, :string
+
+    timestamps()
+  end
+
+  def changeset(user \\ %__MODULE__{}, params) do
+    user
+    |> cast(params, @fields)
+    |> validate_required(@required_fields)
+    |> validate_length(:name, min: 3)
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:cep, is: 8)
+    |> put_pass_hash()
+  end
+
+  defp put_pass_hash(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, Argon2.add_hash(password))
+  end
+
+  defp put_pass_hash(changeset), do: changeset
+end
